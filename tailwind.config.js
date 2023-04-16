6ceb496caea03e6292a7542d0a1e1617eacd0898
Josef Erben <josef@erben.systems>
2023-04-16 19:16:21 +0200

    add space between

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   safelist: [
     {
       pattern:
-        /^(p|m)(t|r|l|b|x|y)-\d|^(w|h)-\d|^flex|^justify|^max-w-|^whitespace|^rounded|^hyphens|^items/,
+        /^(p|m)(t|r|l|b|x|y)-\d|^(w|h)-\d|^flex|^justify|^max-w-|^whitespace|^rounded|^hyphens|^items|^(space)-(x|y)-\d|^mx-auto|^float-right/,
     },
   ],
 };
